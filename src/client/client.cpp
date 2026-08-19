4eed85f18db1481322eb7de2bb155c71ecf35833
Damir <damirmatematic@gmail.com>
2026-08-19 18:09:26 +0300

    Client for connection

---
+++ src/client/client.cpp
@@ -0,0 +1,46 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+using tcp = boost::asio::ip::tcp;
+
+int main()
+{
+    boost::asio::io_context io;
+
+    tcp::socket socket(io);
+
+    tcp::endpoint endpoint{boost::asio::ip::make_address("127.0.0.1"), 8080};
+
+    socket.connect(endpoint);
+
+    std::cout << "Connected\n";
+
+    std::string request;
+
+    for(;;)
+    {
+
+        std::cout<<"> ";
+
+        if(!std::getline(std::cin,request))break;
+
+
+        if (request == "exit")
+            break;
+
+        request += '\n';
+
+        boost::asio::write(
+            socket,
+            boost::asio::buffer(request)
+        );
+
+        std::string response;
+
+        boost::asio::read_until(socket,
+        boost::asio::dynamic_buffer(response),
+        '\n');
+
+        std::cout<<response;
+    }
+}
