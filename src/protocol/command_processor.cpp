fa4dd865d21bfda6a84a8d217be4d61eeaa07056
Damir <damirmatematic@gmail.com>
2026-08-20 05:28:23 +0300

    Added \n

--- src/protocol/command_processor.cpp
+++ src/protocol/command_processor.cpp
@@ -6,7 +6,7 @@ std::string process(const Request &req, std::unordered_map<std::string, std::str
 {
 
     if (req.command.empty())
-        return "ERROR!";
+        return "ERROR!\n";
 
     if (req.command == "SET")
     {
