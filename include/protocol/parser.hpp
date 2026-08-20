b8af5aa1613dbcc9f5ab138ddf21bf520b6a10cd
Damir <damirmatematic@gmail.com>
2026-08-20 04:36:40 +0300

    Standalone func for parsing

---
+++ include/protocol/parser.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "request.hpp"
+#include <string_view>
+
+void parse(Request& req,std::string_view data);
---
+++ src/protocol/parser.cpp
@@ -0,0 +1,33 @@
+#include "parser.hpp"
+
+void parse(Request& req,std::string_view data)
+{
+    std::size_t pos = data.find(' ');
+
+    req.command = data.substr(0, pos);
+
+    if (pos == std::string_view::npos)
+        return;
+
+    data.remove_prefix(pos + 1);
+
+    pos = data.find(' ');
+
+    req.key = data.substr(0, pos);
+
+    if (req.command == "GET" or req.command == "DEL")
+        return;
+
+    if (pos == std::string_view::npos)
+    {
+        req.value = {};
+        return;
+    }
+
+    else
+    {
+        data.remove_prefix(pos + 1);
+        req.value = data;
+        return;
+    }
+}
