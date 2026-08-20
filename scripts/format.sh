4485f008186a2fb2c3e1fbb300b10884b832d1a1
Damir <damirmatematic@gmail.com>
2026-08-20 16:12:38 +0300

    Added tests

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find src include -type f \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
+find src include tests -type f \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
 clang-format -i main.cpp    
