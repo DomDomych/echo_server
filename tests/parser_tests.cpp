706c36a6f91e994435b25a3cf14d4cb1a20dbf0a
Damir <damirmatematic@gmail.com>
2026-08-20 14:49:27 +0300

    Added parser tests

---
+++ tests/parser_tests.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+
+#include "protocol/parser.hpp"
+#include "protocol/request.hpp"
+
+TEST(ParserTest,ParserGet)
+{
+    Request req{};
+
+    parse(req,"GET name");
+
+    EXPECT_EQ(req.command,"GET");
+    EXPECT_EQ(req.key,"name");
+    EXPECT_TRUE(req.value.empty());
+}
+
+TEST(ParserTest,ParserSet)
+{
+    Request req{};
+
+    parse(req,"SET key value");
+
+    EXPECT_EQ(req.command,"SET");
+    EXPECT_EQ(req.key,"key");
+    EXPECT_EQ(req.value,"value");
+}
+
+TEST(ParserTest,ParserDel)
+{
+    Request req{};
+
+    parse(req,"DEL key");
+
+    EXPECT_EQ(req.command,"DEL");
+    EXPECT_EQ(req.key,"key");
+    EXPECT_TRUE(req.value.empty());
+}
+
+TEST(ParserTest,WrongCommand)
+{
+    Request req{};
+
+    parse(req,"set key value");
+    EXPECT_EQ(req.command,"set");
+    EXPECT_EQ(req.key,"key");
+    EXPECT_EQ(req.value,"value");
+}
+
+TEST(ParserTest,ParseValueWithSpaces)
+{
+    Request req{};
+
+    parse(req,"SET key value new");
+    EXPECT_EQ(req.command,"SET");
+    EXPECT_EQ(req.key,"key");
+    EXPECT_EQ(req.value,"value new");
+
+}
