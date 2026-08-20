2f2e288cc7148b6ca34f9f14c867ff3f0fb5773b
Damir <damirmatematic@gmail.com>
2026-08-20 05:08:04 +0300

    Header now with protocol/

--- src/session/session.cpp
+++ src/session/session.cpp
@@ -1,4 +1,8 @@
 #include "session/session.hpp"
+#include "protocol/parser.hpp"
+#include "protocol/command_processor.hpp"
+#include "protocol/request.hpp"
+
 #include <boost/asio.hpp>
 #include <string_view>
 
@@ -7,113 +11,6 @@ using tcp = boost::asio::ip::tcp;
 Session::Session(tcp::socket socket, std::unordered_map<std::string, std::string> &server_storage)
     : socket_(std::move(socket)), server_storage_(server_storage)
 {
-}
-
-void Session::parse(Request &req, std::string_view data)
-{
-    std::size_t pos = data.find(' ');
-
-    req.command = data.substr(0, pos);
-
-    if (pos == std::string_view::npos)
-        return;
-
-    data.remove_prefix(pos + 1);
-
-    pos = data.find(' ');
-
-    req.key = data.substr(0, pos);
-
-    if (req.command == "GET" or req.command == "DEL")
-        return;
-
-    if (pos == std::string_view::npos)
-    {
-        req.value = {};
-        return;
-    }
-
-    else
-    {
-        data.remove_prefix(pos + 1);
-        req.value = data;
-        return;
-    }
-}
-
-void Session::process(Request &req)
-{
-
-    if (req.command.empty())
-        return;
-
-    if (req.command == "SET")
-    {
-
-        if (req.key.empty())
-        {
-            write("No Key!\n");
-            return;
-        }
-        if (req.value.empty())
-        {
-            write("No Value!\n");
-            return;
-        }
-        server_storage_[std::string(req.key)] = std::string(req.value);
-        write("OK!\n");
-
-        return;
-    }
-
-    else if (req.command == "GET")
-    {
-        if (req.key.empty())
-        {
-            write("No Key!\n");
-            return;
-        }
-
-        auto it = server_storage_.find(std::string(req.key));
-
-        if (it == server_storage_.end())
-        {
-            write("No Such Key!\n");
-            return;
-        }
-
-        std::string response = it->second + "\n";
-        write(response);
-        return;
-    }
-
-    else if (req.command == "DEL")
-    {
-        if (req.key.empty())
-        {
-            write("No Key\n");
-            return;
-        }
-
-        auto it = server_storage_.find(std::string(req.key));
-
-        if (it == server_storage_.end())
-        {
-            write("No Such Key!\n");
-            return;
-        }
-
-        server_storage_.erase(std::string(req.key));
-
-        write("OK!\n");
-        return;
-    }
-
-    else
-    {
-        write("ERROR!\n");
-        return;
-    }
 }
 
 void Session::read()
@@ -142,7 +39,7 @@ void Session::read()
         Request req{};
 
         parse(req, temp_data);
-        process(req);
+        write(process(req,server_storage_));
 
         buffer_.erase(0, bytes);
     }
