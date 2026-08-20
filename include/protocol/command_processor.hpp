0fe36b2a2acb088e75abc751bb5779b4b6b3188a
Damir <damirmatematic@gmail.com>
2026-08-20 04:35:07 +0300

    Processing command is now standalone function

---
+++ include/protocol/command_processor.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "request.hpp"
+#include <string>
+#include <unordered_map>
+
+std::string process(
+    const Request& req,
+    std::unordered_map<std::string,std::string>& storage
+);
